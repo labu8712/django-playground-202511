604a2b0eca174e8f95f5419fb9ad0fa4416499d6
Arthur Chang <arthurc0102@gmail.com>
2025-11-22 13:30:44 +0800

    2025/11/22 - 008

--- practices/views.py
+++ practices/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def hello_world(request):
     return render(request, "practices/hello.html")
+
+
+def greeting(request):
+    name = "Django"
+    return render(request, "practices/greeting.html", {"name": name})
