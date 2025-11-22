885d2c74e3c7ec9039a5aad17eb5bd18c3776d09
Arthur Chang <arthurc0102@gmail.com>
2025-11-22 11:51:38 +0800

    2025/11/22 - 005

--- practices/views.py
+++ practices/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def hello_world(request):
-    return HttpResponse("<h1>Hello, World!</h1>")
+    return render(request, "practices/hello.html")
