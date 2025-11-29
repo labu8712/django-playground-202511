1838edd35164fc254b7eec4fda02564221406f64
Arthur Chang <arthurc0102@gmail.com>
2025-11-29 10:51:18 +0800

    2025/11/29 - 003

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Article(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
