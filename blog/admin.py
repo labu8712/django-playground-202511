71006c6529c0209af282a0ad25b716de95dfdfb3
Arthur Chang <arthurc0102@gmail.com>
2025-11-29 14:44:55 +0800

    2025/11/29 - 007

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from blog.models import Article, Author, Tag
 
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ["name", "email", "created_at"]
+
+
 admin.site.register(Article)
-admin.site.register(Author)
 admin.site.register(Tag)
