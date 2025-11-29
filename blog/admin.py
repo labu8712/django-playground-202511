e75f61c79f08f6c586e9b1ffa76786391ee853a3
Arthur Chang <arthurc0102@gmail.com>
2025-11-29 14:41:11 +0800

    2025/11/29 - 006

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Article, Author, Tag
+
+admin.site.register(Article)
+admin.site.register(Author)
+admin.site.register(Tag)
