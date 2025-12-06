dca7f9d4e04f55a9829cffe20a3874299f1cc819
Arthur Chang <arthurc0102@gmail.com>
2025-12-06 10:36:28 +0800

    2025/12/06 - 003

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from blog import views
 
 urlpatterns = [
     path("articles/", views.article_list, name="article_list"),
+    path("articles/<int:article_id>/", views.article_detail, name="article_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,8 @@ from blog.models import Article
 def article_list(request):
     articles = Article.objects.all()
     return render(request, "blog/article_list.html", {"articles": articles})
+
+
+def article_detail(request, article_id):
+    article = Article.objects.get(id=article_id)
+    return render(request, "blog/article_detail.html", {"article": article})
