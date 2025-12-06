fc5121246c5091a4d832afaaec50c2be165b8b84
Arthur Chang <arthurc0102@gmail.com>
2025-12-06 09:53:48 +0800

    2025/12/06 - 001

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from blog.models import Article
+
+
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, "blog/article_list.html", {"articles": articles})
--- core/urls.py
+++ core/urls.py
@@ -21,4 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("practices/", include("practices.urls")),
+    path("blog/", include("blog.urls")),
 ]
