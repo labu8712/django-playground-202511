d310c823080d0b812b1251add3795e8a95692285
Arthur Chang <arthurc0102@gmail.com>
2025-12-13 11:56:06 +0800

    2025/12/13 - 008

--- blog/views.py
+++ blog/views.py
@@ -9,5 +9,8 @@ def article_list(request):
 
 
 def article_detail(request, article_id):
-    article = get_object_or_404(Article, id=article_id)
+    article = get_object_or_404(
+        Article.objects.select_related("author").prefetch_related("tags"),
+        id=article_id,
+    )
     return render(request, "blog/article_detail.html", {"article": article})
