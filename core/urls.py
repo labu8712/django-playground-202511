b0e1652c059e8c44ba7cd24eb7b4613188ac723b
Arthur Chang <arthurc0102@gmail.com>
2025-12-27 11:47:24 +0800

    2025/12/27 - 005

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import include, path
+from django.urls import include, path, reverse_lazy
 
 from core import views
 
@@ -17,6 +17,21 @@ auth_urlpatterns = [
         name="logout",
     ),
     path("register/", views.register, name="register"),
+    path(
+        "password-change/",
+        auth_views.PasswordChangeView.as_view(
+            template_name="registration/password_change.html",
+            success_url=reverse_lazy("auth:password_change_done"),
+        ),
+        name="password_change",
+    ),
+    path(
+        "password-change/done/",
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name="registration/password_change_done.html"
+        ),
+        name="password_change_done",
+    ),
 ]
 
 urlpatterns = [
