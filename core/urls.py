33722bfb58612585006409b0de4431b7c3fe8cf8
Arthur Chang <arthurc0102@gmail.com>
2025-12-27 13:17:45 +0800

    2025/12/27 - 007

--- core/urls.py
+++ core/urls.py
@@ -32,6 +32,36 @@ auth_urlpatterns = [
         ),
         name="password_change_done",
     ),
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="registration/password_reset.html",
+            success_url=reverse_lazy("auth:password_reset_done"),
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="registration/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="registration/password_reset_confirm.html",
+            success_url=reverse_lazy("auth:password_reset_complete"),
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-reset/complete/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="registration/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 ]
 
 urlpatterns = [
