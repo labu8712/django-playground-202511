7bedc6dcf28f662c30b8492a69f152d3160ce1f9
Arthur Chang <arthurc0102@gmail.com>
2025-12-27 14:06:54 +0800

    2025/12/27 - 009

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import permission_required
 from django.shortcuts import get_object_or_404, redirect, render
 
 from blog.filters import ArticleFilter
@@ -23,7 +23,7 @@ def article_detail(request, article_id):
     return render(request, "blog/article_detail.html", {"article": article})
 
 
-@login_required
+@permission_required("blog.add_article", raise_exception=True)
 def article_create(request):
     form = ArticleForm(request.POST or None)
     if form.is_valid():
@@ -34,7 +34,7 @@ def article_create(request):
     return render(request, "blog/article_create.html", {"form": form})
 
 
-@login_required
+@permission_required("blog.change_article", raise_exception=True)
 def article_edit(request, article_id):
     article = get_object_or_404(Article, id=article_id)
     form = ArticleForm(request.POST or None, instance=article)
@@ -46,7 +46,7 @@ def article_edit(request, article_id):
     return render(request, "blog/article_edit.html", {"form": form, "article": article})
 
 
-@login_required
+@permission_required("blog.delete_article", raise_exception=True)
 def article_delete(request, article_id):
     article = get_object_or_404(Article, id=article_id)
 
@@ -58,7 +58,7 @@ def article_delete(request, article_id):
     return render(request, "blog/article_delete.html", {"article": article})
 
 
-@login_required
+@permission_required("blog.delete_article", raise_exception=True)
 def article_bulk_delete(request):
     if request.method == "POST":
         article_ids = request.POST.getlist("article_ids")
