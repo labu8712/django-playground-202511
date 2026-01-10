73f32e544c4d6e09591a05ee32cd79966a116db2
Arthur Chang <arthurc0102@gmail.com>
2026-01-10 10:13:42 +0800

    2026/01/10 - 002

--- core/settings.py
+++ core/settings.py
@@ -136,6 +136,17 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATIC_ROOT = BASE_DIR / "assets"
+
+# Media files (User uploads)
+
+MEDIA_URL = "media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path, reverse_lazy
@@ -74,4 +75,9 @@ urlpatterns = [
 if settings.DEBUG:
     from debug_toolbar.toolbar import debug_toolbar_urls
 
-    urlpatterns = [*urlpatterns, *debug_toolbar_urls()]
+    urlpatterns = [
+        *urlpatterns,
+        *debug_toolbar_urls(),
+        *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+        *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+    ]
