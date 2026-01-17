ba73a7329bcf307ba5283fdcc684a23d08b22d32
Arthur Chang <arthurc0102@gmail.com>
2026-01-17 14:37:22 +0800

    2026/01/17 - 011

--- blog/drf_views.py
+++ blog/drf_views.py
@@ -1,62 +1,50 @@
 from rest_framework import status
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from blog.models import Article
 from blog.serializers import ArticleSerializer
 
 
-class ArticleListAPIView(APIView):
+class ArticleListAPIView(GenericAPIView):
     """文章列表 API"""
 
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
     def get(self, request):
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles, many=True)
+        articles = self.get_queryset()
+        serializer = self.get_serializer(articles, many=True)
         return Response(serializer.data)
 
     def post(self, request):
-        serializer = ArticleSerializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             serializer.save(created_by=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class ArticleDetailAPIView(APIView):
+class ArticleDetailAPIView(GenericAPIView):
     """文章詳情 API"""
 
-    def get_object(self, pk):
-        return Article.objects.get(pk=pk)
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
 
     def get(self, request, pk):
-        try:
-            article = self.get_object(pk)
-        except Article.DoesNotExist:
-            return Response(
-                {"detail": "找不到該文章"}, status=status.HTTP_404_NOT_FOUND
-            )
-        serializer = ArticleSerializer(article)
+        article = self.get_object()
+        serializer = self.get_serializer(article)
         return Response(serializer.data)
 
     def put(self, request, pk):
-        try:
-            article = self.get_object(pk)
-        except Article.DoesNotExist:
-            return Response(
-                {"detail": "找不到該文章"}, status=status.HTTP_404_NOT_FOUND
-            )
-        serializer = ArticleSerializer(article, data=request.data)
+        article = self.get_object()
+        serializer = self.get_serializer(article, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
-        try:
-            article = self.get_object(pk)
-        except Article.DoesNotExist:
-            return Response(
-                {"detail": "找不到該文章"}, status=status.HTTP_404_NOT_FOUND
-            )
+        article = self.get_object()
         article.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
