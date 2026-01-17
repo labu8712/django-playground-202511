8ac22f0877018c1cbf4ff7b966f5bcad6f3c785c
Arthur Chang <arthurc0102@gmail.com>
2026-01-17 09:36:25 +0800

    2026/01/17 - 001

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path, reverse_lazy
+from django.views.generic import RedirectView
 
 from core import views
 
@@ -66,6 +67,7 @@ auth_urlpatterns = [
 ]
 
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name="blog:article_list"), name="root"),
     path("admin/", admin.site.urls),
     path("practices/", include("practices.urls")),
     path("blog/", include("blog.urls")),
