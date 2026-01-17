279246e8c966e976d0b1176ca0ce8fdee22a5db3
Arthur Chang <arthurc0102@gmail.com>
2026-01-17 15:05:12 +0800

    2026/01/17 - 013

--- blog/drf_views.py
+++ blog/drf_views.py
@@ -1,45 +1,22 @@
-from rest_framework import mixins
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from blog.models import Article
 from blog.serializers import ArticleSerializer
 
 
-class ArticleListAPIView(
-    mixins.ListModelMixin, mixins.CreateModelMixin, GenericAPIView
-):
+class ArticleListAPIView(ListCreateAPIView):
     """文章列表 API"""
 
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         """在建立物件時設定 created_by"""
         serializer.save(created_by=self.request.user)
 
 
-class ArticleDetailAPIView(
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    GenericAPIView,
-):
+class ArticleDetailAPIView(RetrieveUpdateDestroyAPIView):
     """文章詳情 API"""
 
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
