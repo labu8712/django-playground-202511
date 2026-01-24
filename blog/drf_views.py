268b9a97c857ff0d492e9ecfea504d8c4dad53e2
Arthur Chang <arthurc0102@gmail.com>
2026-01-24 10:04:30 +0800

    2026/01/24 - 003

--- blog/drf_views.py
+++ blog/drf_views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
 from rest_framework.viewsets import ModelViewSet
 
@@ -11,6 +13,10 @@ class ArticleViewSet(ModelViewSet):
     permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = ["title", "content"]
+    ordering_fields = ["created_at", "title"]
+    filterset_fields = ["is_published", "author"]
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
