8425853c32149811a7ed099f622b700fb8428b61
Arthur Chang <arthurc0102@gmail.com>
2026-01-24 11:14:15 +0800

    2026/01/24 - 007

--- blog/ninja.py
+++ blog/ninja.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from ninja import Router
 
 from blog.models import Article
-from blog.schemas import ArticleOut
+from blog.schemas import ArticleIn, ArticleOut
 
 router = Router()
 
@@ -15,3 +15,9 @@ def list_articles(request):
 @router.get("/articles/{article_id}", response=ArticleOut)
 def get_article(request, article_id: int):
     return get_object_or_404(Article, id=article_id)
+
+
+@router.post("/articles", response={201: ArticleOut})
+def create_article(request, payload: ArticleIn):
+    article = Article.objects.create(**payload.dict())
+    return 201, article
