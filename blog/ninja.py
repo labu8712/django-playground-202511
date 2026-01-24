e7934819a687cf7627a2878a82201f7997713864
Arthur Chang <arthurc0102@gmail.com>
2026-01-24 11:45:04 +0800

    2026/01/24 - 008

--- blog/ninja.py
+++ blog/ninja.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from ninja import Router
+from ninja import PatchDict, Router
 
 from blog.models import Article
 from blog.schemas import ArticleIn, ArticleOut
@@ -21,3 +21,34 @@ def get_article(request, article_id: int):
 def create_article(request, payload: ArticleIn):
     article = Article.objects.create(**payload.dict())
     return 201, article
+
+
+@router.put("/articles/{article_id}", response=ArticleOut)
+def update_article(request, article_id: int, payload: ArticleIn):
+    article = get_object_or_404(Article, id=article_id)
+    for attr, value in payload.dict().items():
+        setattr(article, attr, value)
+
+    article.save()
+    return article
+
+
+@router.patch("/articles/{article_id}", response=ArticleOut)
+def partial_update_article(
+    request,
+    article_id: int,
+    payload: PatchDict[ArticleIn],
+):
+    article = get_object_or_404(Article, id=article_id)
+    for attr, value in payload.items():
+        setattr(article, attr, value)
+
+    article.save()
+    return article
+
+
+@router.delete("/articles/{article_id}", response={204: None})
+def delete_article(request, article_id: int):
+    article = get_object_or_404(Article, id=article_id)
+    article.delete()
+    return 204, None
